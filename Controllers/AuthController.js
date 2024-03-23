7daf3d318c4456ebaa5c47c3c3a6017d296e58f9
Aml <amlmahdawy21@gmail.com>
2024-03-23 06:36:41 +0200

    editing way of extracting body to fit axios

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -11,11 +11,11 @@ async function FoundUser(email) {
 
 let register = async (req, res) => {
 
-    var body = req.body;
+    var body = req.body.data;
 
 
     let foundUser = await FoundUser(body.email);
-    if (foundUser) return res.status(404).send({ message: "email already registered" });
+    if (foundUser) return res.status(404).send({ message: false });
 
 
     var salt = await bcrypt.genSalt(10);
@@ -28,36 +28,37 @@ let register = async (req, res) => {
     await newUser.save()
         .then()
         .catch((err) => { res.json({ message: err }) });
-    res.status(201).send({ message: "added" });
+    res.status(201).send({ message: true });
 
 
 }
 let login = async (req, res) => {
-    var body = req.body;
+    var body = req.body.data;
+    
     if (!body.gmail) {
         body.email = body.email.toLowerCase();
         let foundUser = await FoundUser(body.email);
-        if (!foundUser) return res.status(404).send({ message: "invalid email or password" })
+        if (!foundUser) return res.status(200).send({ message: false })
 
         var passwordValid = await bcrypt.compare(body.password, foundUser.password);
 
-        if (!passwordValid) return res.status(404).send({ message: "invalid email or password" });
+        if (!passwordValid) return res.status(200).send({ message: false});
 
         var token = jwt.sign({ id: foundUser._id, email: foundUser.email, isAdmin: foundUser.isAdmin }, "secret");
 
         res.header("x-auth-token", token);
-        res.status(200).send({ token: token });
+        res.status(200).send({ token: token ,message:true});
     } else {
         body.email = body.email.toLowerCase();
         let foundUser = await FoundUser(body.email);
-        if (!foundUser) return res.status(404).send({ message: false })
+        if (!foundUser) return res.status(200).send({ message: false })
         var token = jwt.sign({
             id: foundUser._id,
             email: foundUser.email,
             isAdmin: foundUser.isAdmin
         }, "secret");
         res.header("x-auth-token", token);
-        res.status(200).send({ token: token });
+        res.status(200).send({ token: token ,message:true});
 
     }
 
